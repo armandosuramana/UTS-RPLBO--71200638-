bbe6acccae188d84776bd9a7b1b58a4e7814337e
armandosuramana <79094911+armandosuramana@users.noreply.github.com>
2022-10-24 09:30:07 +0700

    Add files via upload

---
+++ TTS-71200638/Vehicle/src/main/java/com/rplbo/MotorVehicle.java
@@ -0,0 +1,12 @@
+package com.rplbo;
+
+public class MotorVehicle {
+    private String vehicleName;
+    private Engine engine;
+    private Tire tire;
+    private Wheel wheel;
+    private Transmition transmition;
+
+
+
+}
---
+++ TTS-71200638/Vehicle/src/main/java/com/rplbo/Vehicle.java
@@ -0,0 +1,8 @@
+package com.rplbo;
+
+public class Vehicle {
+    public void backward();
+    public void brake();
+    public void forward();
+    public
+}
