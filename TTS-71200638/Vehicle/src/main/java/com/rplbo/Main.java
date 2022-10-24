bd66032ccefbdb73e5b929cf8bea0fa82d4bdd1b
armandosuramana <79094911+armandosuramana@users.noreply.github.com>
2022-10-24 09:57:44 +0700

    Delete TTS-71200638 directory

--- TTS-71200638/Vehicle/src/main/java/com/rplbo/Main.java
+++
@@ -1,7 +0,0 @@
-package com.rplbo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- TTS-71200638/Vehicle/src/main/java/com/rplbo/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package com.rplbo;
-
-public class Vehicle {
-    public void backward();
-    public void brake();
-    public void forward();
-    public
-}
